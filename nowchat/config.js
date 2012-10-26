5ab528b6781db0f295663a16a7e76869dc8eb286
J.R. Patten <jr@juanpatten.com>
2012-10-26 00:32:10 -0600

    existsSync

--- nowchat/config.js
+++ nowchat/config.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
 
-if (fs.exists('/home/dotcloud/environment.json')) {
+if (fs.existsSync('/home/dotcloud/environment.json')) {
   var env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json'))
   exports = module.exports = {
     mongo: env['DOTCLOUD_MONGO_MONGODB_URL']
