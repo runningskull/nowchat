f88cd075be280b8fda56f91aba67d519630472a1
J.R. Patten <jr@juanpatten.com>
2012-10-26 00:17:36 -0600

    add mongo

--- nowchat/config.js
+++ nowchat/config.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 if (fs.exists('/home/dotcloud/environment.json')) {
   var env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json'))
   exports = module.exports = {
-    mongo: env['DOTCLOUD_MONGO_MONGO_URL']
+    mongo: env['DOTCLOUD_MONGO_MONGODB_URL']
   }
 } else {
   exports = module.exports = {}
